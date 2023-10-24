6fe0edcfaed98c245d241c30c2e7faf5982eddf7
Milena Mello <milenamello625@gmail.com>
2023-10-24 22:17:30 +0300

    T-Shirt Info Section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// Name Field:
 const name = document.querySelector('#name').focus();
+
+// Job Role Section:
 let jobRoleText = document.querySelector('#other-job-role').style.display = "none";
 
 let jobRole = document.querySelector('#title');
@@ -8,4 +11,12 @@ jobRole.addEventListener('change', (e) => {
      } else {
         document.querySelector('#other-job-role').style.display = 'none';
      }
-})
+});
+
+// T-Shirt Info Section:
+let color = document.querySelector('#color').disabled = true;
+let designElement = document.querySelector('#design');
+designElement.addEventListener('change', (e) {
+    color = document.querySelector('#color').disabled = false;
+});
+
