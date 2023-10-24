723e36cceaad5b222e56e568edaab29b803e1fec
Milena Mello <milenamello625@gmail.com>
2023-10-25 00:03:10 +0300

    Activities Section

--- js/script.js
+++ js/script.js
@@ -16,7 +16,44 @@ jobRole.addEventListener('change', (e) => {
 // T-Shirt Info Section:
 let color = document.querySelector('#color').disabled = true;
 let designElement = document.querySelector('#design');
-designElement.addEventListener('change', (e) {
-    color = document.querySelector('#color').disabled = false;
+let colorOptions = document.querySelectorAll("option[data-theme]");
+
+designElement.addEventListener('change', () => {
+document.querySelector('#color').disabled = false;
+    
+    for(let i=0; i < colorOptions.length; i++){
+        if(designElement.value !== colorOptions[i].getAttribute("data-theme")){
+            colorOptions[i].hidden = true;
+            colorOptions[i].disabled = true;
+        } else {
+            colorOptions[i].hidden = false;
+            colorOptions[i].disabled = false;
+        }
+    }
 });
+
+// Activities Section:
+const activities = document.querySelector("#activities");
+let activity = document.querySelectorAll("input[type='checkbox']")
+let totalCost = document.querySelector("#activities-cost");
+let totalPrice = 0;
+
+ activities.addEventListener('change', (event) => {
+        if(event.target.checked === true) {
+            let dataCost = event.target.getAttribute("data-cost");
+            dataCost = +dataCost; // transform string to a number.
+            totalPrice += dataCost;
+            totalCost.innerHTML = `Total: $${totalPrice}`
+        } 
+        if (event.target.checked === false) {
+            let dataCost = event.target.getAttribute("data-cost");
+            dataCost = +dataCost; // transform string to a number.
+            totalPrice -= dataCost;
+            totalCost.innerHTML = `Total: $${totalPrice}`
+        }
+    }
+);
+
+
+
 
