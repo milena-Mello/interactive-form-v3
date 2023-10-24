9a694a4c1402aef450d287b031b812e3726a0c5c
Milena Mello <milenamello625@gmail.com>
2023-10-24 21:21:28 +0300

    Job Role Section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('test');
+const name = document.querySelector('#name').focus();
+let jobRoleText = document.querySelector('#other-job-role').style.display = "none";
+
+let jobRole = document.querySelector('#title');
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value == 'other') {
+        document.querySelector('#other-job-role').style.display = 'block';
+     } else {
+        document.querySelector('#other-job-role').style.display = 'none';
+     }
+})
